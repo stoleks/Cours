a85ddf6bc61e89dbf580dfe27cbfb156384c2047
stoleks <stoleks@hotmail.fr>
2023-12-21 01:41:00 +0100

    Mise à jour des figures sur les corps chauds

--- images/thermodynamique/emission_corps_chaud.py
+++ images/thermodynamique/emission_corps_chaud.py
@@ -14,22 +14,54 @@ def luminance (longueurOnde, temperature):
     norm = 2*h*c*c / (lambd**5)
     return norm / (np.exp (E) - 1)
 
+def Wien (temperature):
+    return 2.9e-3 / temperature
 
-# longueur d'onde
-lambd = np.linspace (0.1, 4, 200)
 
-# trace du graphique
-plt.xlim ((0, 4))
-plt.plot (lambd, luminance (lambd, 1000), lw=5, label="                          ")
-plt.plot (lambd, luminance (lambd, 3000), lw=5, label=" ")
-plt.plot (lambd, luminance (lambd, 4000), lw=5, label=" ")
-plt.plot (lambd, luminance (lambd, 5000), lw=5, label=" ")
+def traceCorpsChaud ():
+    # longueur d'onde
+    lambd = np.linspace (0.1, 4, 200)
+    plt.xlim ((0, 4))
+    
+    plt.plot (lambd, luminance (lambd, 1000), lw=5, label="                          ")
+    plt.plot (lambd, luminance (lambd, 3000), lw=5, label=" ")
+    plt.plot (lambd, luminance (lambd, 4000), lw=5, label=" ")
+    plt.plot (lambd, luminance (lambd, 5000), lw=5, label=" ")
+    plt.legend()
 
-plt.xlabel("$\lambda$ ($\mu$m)")
-plt.ylabel("Flux lumineux")
-plt.legend()
+    plt.xlabel("$\lambda$ ($\mu$m)")
+    plt.ylabel("Flux lumineux")
+    
+    plt.rcParams["figure.figsize"] = (6, 6)
+    plt.savefig ('luminance_corps_chaud.png', dpi=300, transparent=True)
+    plt.show ()
+    
+    
+def traceCorpsChaudT3000 ():
+    lambd = np.linspace (0.1, 4, 200)
+    plt.xlim ((0, 4))
+    plt.plot (lambd, luminance (lambd, 4000), lw=5)
 
-# parametre de la grille
-plt.rcParams["figure.figsize"] = (6, 6)
-plt.savefig ('luminance_corps_chaud.png', dpi=300, transparent=True)
-plt.show ()
+    plt.xlabel("$\lambda$ ($\mu$m)")
+    plt.ylabel("Flux lumineux")
+    
+    plt.rcParams["figure.figsize"] = (6, 6)
+    plt.savefig ('luminance_corps_chaud_T3000.png', dpi=300, transparent=True)
+    plt.show ()
+    
+    
+def traceWien ():
+    T = np.linspace (300, 5000, 200)
+    plt.plot (T, Wien (T), lw=5)
+
+    plt.xlabel("$T$ (K))")
+    plt.ylabel("$\lambda$ (m)")
+
+    plt.rcParams["figure.figsize"] = (6, 6)
+    plt.savefig ('loi_wien.png', dpi=300, transparent=True)
+    plt.show ()
+    
+    
+traceCorpsChaud()
+traceCorpsChaudT3000()
+traceWien()
